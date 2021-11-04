333a31587ce391c4b178b1d4ba9e227dbb0edbc1
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2021-11-04 17:24:39 -0400

    Add files via upload

---
+++ erc721.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+ 
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+ 
+contract newNFT is NFTokenMetadata, Ownable {
+ 
+  constructor() {
+    nftName = "Synth NFT";
+    nftSymbol = "SYN";
+  }
+ 
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+  }
+ 
+}
+
